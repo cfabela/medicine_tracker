7d506fab2bf2b6daae3b24bfc557a0ae484358a7
Carlos Fabela <cfabela@itexico.com>
2019-05-21 22:06:07 -0500

    Adding Service/Navigation implementatio to the project

---
+++ MedicineTracker/MedicineTracker/Services/INavigationService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace MedicineTracker.Services
+{
+    public interface INavigationService
+    {
+        //Async removes the most recent Page from the navigation stack
+        Task<Page> RemoveViewFromStack();
+
+        //Navigate to a particular ViewModel within out MVVM Model
+        Task NaviateTo<TVM>()
+            where TVM : BaseViewModel;
+    }
+}
---
+++ MedicineTracker/MedicineTracker/Services/NavigationService.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+using MedicineTracker.Services;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(NavigationService))]
+namespace MedicineTracker.Services
+{
+    public class NavigationService : INavigationService
+    {
+        public INavigation XFNavigation { get; set; }
+        readonly IDictionary<Type, Type> _viewMapping = new Dictionary<Type, Type>();
+
+       //Register our ViewModel and View within our Dictionary
+       public void RegisterViewMapping(Type viewModel, Type view)
+        {
+            _viewMapping.Add(viewModel, view);
+        }
+
+        //Removes the most recent Page from the navigation stack.
+        public Task<Page> RemoveViewFromStack()
+        {
+            return XFNavigation.PopAsync();
+        }
+
+        //Navigates navigates to a specific ViewModel
+        public async Task NavifateTo<TVM>()
+            where TVM : BaseViewModel
+        {
+            await NavigateToView(typeof(TVM));
+
+            if (XFNavigation.NavigationStack.Last().BindingContext is BaseViewModel)
+                await ((BaseViewModel)(XFNavigation.NavigationStack.Last().BindingContext)).Init();
+        }
+
+        //Navigates to a specific ViewModel within our dictionary viewMapping
+        public async Task NavigateToView(Type viewModelType)
+        {
+            Type viewType;
+
+            if (!_viewMapping.TryGetValue(viewModelType, out viewType))
+                throw new ArgumentException("No view found in View Mapping for ");
+
+
+            var constructor = viewType.GetTypeInfo().DeclaredConstructors.
+                FirstOrDefault(dc => !dc.GetParameters().Any());
+
+            var view = constructor.Invoke(null) as Page;
+            await XFNavigation.PushAsync(view, true);
+        }
+
+
+
+
+
+
+
+
+    }
+}
