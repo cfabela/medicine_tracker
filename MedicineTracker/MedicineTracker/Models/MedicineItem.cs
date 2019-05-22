9d98ea772700eb9e6572f11314cb40002e04f42e
Carlos Fabela <cfabela@itexico.com>
2019-05-21 22:33:55 -0500

    Adding files for database and database schema

---
+++ MedicineTracker/MedicineTracker/Models/MedicineItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using SQLite;
+
+namespace MedicineTracker.Models
+{
+    public class MedicineItem
+    {
+       [PrimaryKey, AutoIncrement]
+       public int Id { get; set; }
+        public string BrandName { get; set; }
+        public string Description { get; set; }
+        public string SideEffects { get; set; }
+        public string Dosage { get; set; }
+
+        public DateTime DateDoseTaken { get; set; }
+        public TimeSpan TimeDoseTaken { get; set; }
+    }
+}
