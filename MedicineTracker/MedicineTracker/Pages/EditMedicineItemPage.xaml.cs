9130bb64ca12bb0aa08e18439ba3dcc80ca300d5
Carlos Fabela <cfabela@itexico.com>
2019-05-23 21:25:14 -0500

    Adding xaml for interfaces

---
+++ MedicineTracker/MedicineTracker/Pages/EditMedicineItemPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace MedicineTracker.Pages
+{
+    public partial class EditMedicineItemPage : ContentPage
+    {
+        public EditMedicineItemPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MedicineTracker/MedicineTracker/Pages/MedicineListPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace MedicineTracker.Pages
+{
+    public partial class MedicineListPage : ContentPage
+    {
+        public MedicineListPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
